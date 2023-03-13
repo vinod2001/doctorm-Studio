57a0acb1a1c0778b431b10ef739bbf4694a83f44
vinod <vinod.2001@gmail.com>
2023-03-14 00:49:42 +0530

    first commit studio

---
+++ schemas/post.ts
@@ -0,0 +1,106 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Predefine Pages',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    // defineField({
+    //   name: 'author',
+    //   title: 'Author',
+    //   type: 'reference',
+    //   to: {type: 'author'},
+    // }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'findFrames',
+      title: 'Find Frames',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    
+    defineField({
+      name: 'topCategoriesHeading',
+      title: 'Top Categories Heading',
+      type: 'string',
+    }),
+    defineField({
+      name: 'topCategories',
+      title: 'Top Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'topBrandsHeading',
+      title: 'Top Brands Heading',
+      type: 'string',
+    }),
+    defineField({
+      name: 'topBrands',
+      title: 'Top Brands',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'largeBanners',
+      title: 'Large Banners',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'biggerBanners',
+      title: 'Bigger Banners',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+
+  // preview: {
+  //   select: {
+  //     title: 'title',
+  //     author: 'author.name',
+  //     media: 'mainImage',
+  //   },
+  //   prepare(selection) {
+  //     const {author} = selection
+  //     return {...selection, subtitle: author && `by ${author}`}
+  //   },
+  // },
+})
